48192772f430c18a63b604b135016db686b3b295
Rico Jones <rico@toasterlovin.com>
2017-06-24 04:41:29 +0000

    Implement CLI support for basic operations
    - Adds support for basic operations from the CLI
    - Adds test of CLI using ruby PTY library
    - Adds test helper methods to clean up vagaries of interacting
      with PTY

--- lib/rpn_party/cli.rb
+++ lib/rpn_party/cli.rb
@@ -1,7 +1,21 @@
+require 'rpn_party/calculator'
+
 module RPNParty
   class CLI
     def initialize
+      @calc = RPNParty::Calculator.new
+
       puts 'Welcome to RPNParty!'
+      print '> '
+
+      loop do
+        input = gets.chomp
+        if input
+          @calc.evaluate(input)
+          puts "#=> #{@calc.result}"
+          print '> '
+        end
+      end
     end
   end
 end
--- test/test_cli.rb
+++ test/test_cli.rb
@@ -1,15 +1,17 @@
 require 'minitest/autorun'
 require 'rpn_party/cli'
+require 'open3'
+require 'pty'
 
 class CLITest < Minitest::Test
   def test_basic_operation
-    IO.popen('bin/rpn_party', 'r+') do |console|
-      response = console.gets.chomp
-      assert_equal 'Welcome to RPNParty!', response
+    PTY.spawn('bin/rpn_party') do |output, input, pid|
+      pty = [output, input, pid]
 
-      console.puts '2 3 +'
-      response = console.gets.chomp
-      assert_equal '5', response
+      assert_equal 'Welcome to RPNParty!', get_response(pty)
+
+      send_command pty, '2 3 +'
+      assert_equal '5.0', get_result(pty)
     end
   end
 
@@ -23,5 +25,20 @@ class CLITest < Minitest::Test
 
   def test_quitting_on_eof
     flunk
+  end
+
+  private
+
+  def get_response(pty)
+    pty[0].gets.chomp
+  end
+
+  def send_command(pty, command)
+    pty[1].puts command
+    pty[0].gets
+  end
+
+  def get_result(pty)
+    get_response(pty).gsub('#=> ', '')
   end
 end
